1f58fcd81ea0a08be65d9aca36900a30d2bf1980
maardje <maartje.snoeckhenkemans@gmail.com>
2018-01-04 12:09:46 +0100

    feat(tablePOC): proof of concept for a simple table without formatting

---
+++ src/components/table.js
@@ -0,0 +1,61 @@
+import React, { Component, ReactPropTypes } from 'react';
+
+// typescript mis ik nu 
+
+/* 
+    interface TableProps {
+        columnNames: string[];
+        values: object[]
+    }
+*/
+
+const columnNames = ['Coin', 'Amount', 'Buy', 'Worth', 'Gain/Loss'];
+const values = [
+    {
+        'Coin': 'XRP',
+        'Amount': '2000',
+        'Buy': '0.20',
+        'Worth': '2',
+        'Gain/Loss': 'alotta'
+    },
+    {
+        'Coin': 'XVG',
+        'Amount': '15000',
+        'Buy': '0.0048',
+        'Worth': '0.13',
+        'Gain/Loss': '100miljoen'
+    }
+
+]
+
+
+export default class Table extends Component {
+
+    render() {
+        return (
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            {
+                                columnNames.map(columnName => <th>{columnName}</th>)
+                            }
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            {
+                                Object.values(values[0]).map(item => <td>{item}</td>)
+                            }
+                        </tr>
+                        <tr>
+                            {
+                                Object.values(values[1]).map(item => <td>{item}</td>)
+                            }
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
